7e0c57bf2210fd1e727aa71d8e66837987bc60e5
Daphne Looije <Lje@cqm.nl>
2021-01-27 18:33:35 +0100

    My RomanNumeral implementation and tests

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -18,6 +18,51 @@ public class RomanNumeral {
         map.put('M', 1000);
     }
 
+    public int convertDaphne(String s) {
+        //Convert a roman numeral string to a decimal
+        //There are two rules:
+        // 1. Read from left to right
+        // 2. If the character at position i is less than the character at position i+1, subtract it
+
+        String temp = s;
+        int result = 0;
+        while (temp.length() > 0) {
+            //If there is only one character, look it up and add it to the result
+            if (temp.length() == 1) {
+                result += map.get(temp.charAt(0));
+                break;
+            }
+            //If there is more than one character, but the first two characters are the same, the string is invalid
+            else if (map.get(temp.charAt(1)) == map.get(temp.charAt(0)))
+            {
+                result = -1;
+                break;
+            }
+            //If the first character is less than the second, we have to subtract
+            else if (map.get(temp.charAt(0)) < map.get(temp.charAt(1))) {
+                //But if the difference between the characters is represented by some other character, the string is invalid
+                if (map.containsValue(map.get(temp.charAt(1)) - map.get(temp.charAt(0))))
+                {
+                    result = -1;
+                    break;
+                }
+                //Otherwise we can add the difference between the two characters to the result and remove the characters from the string
+                else
+                {
+                    result += map.get(temp.charAt(1));
+                    result -= map.get(temp.charAt(0));
+                    temp = temp.substring(2);
+                }
+            }
+            //Final situation, just add the value of the first character to the result and remove the character from the string
+            else {
+                result += map.get(temp.charAt(0));
+                temp = temp.substring(1);
+            }
+        }
+        return result;
+    }
+
     public int convert(String s) {
 
         int convertedNumber = 0;
---
+++ src/test/java/tudelft/roman/RomanNumeralTestDaphne.java
@@ -0,0 +1,49 @@
+package tudelft.roman;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralTestDaphne {
+    @Test
+    public void singleNumberDaphne() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "X";
+        Assertions.assertEquals(10, roman.convertDaphne(input));
+    }
+
+    @Test
+    public void doubleNumberAdditiveDaphne() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "XV";
+        Assertions.assertEquals(15, roman.convertDaphne(input));
+    }
+
+    @Test
+    public void doubleNumberSubtractDaphne() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "IX";
+        Assertions.assertEquals(9, roman.convertDaphne(input));
+    }
+
+    @Test
+    public void tripleNumberAddAndSubtractDaphne() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "XIV";
+        Assertions.assertEquals(14, roman.convertDaphne(input));
+    }
+
+    @Test
+    public void wrongInput() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "VX";
+        Assertions.assertEquals(-1, roman.convertDaphne(input));
+    }
+
+
+    @Test
+    public void wrongInput2() {
+        RomanNumeral roman = new RomanNumeral();
+        String input = "XXC";
+        Assertions.assertEquals(-1, roman.convertDaphne(input));
+    }
+}

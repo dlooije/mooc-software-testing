40ea4982e87cf609ef49469bd88fa8900f0692ee
Daphne Looije <Lje@cqm.nl>
2021-02-12 14:39:49 +0100

    CountLetters tests and bugfix

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -17,4 +17,28 @@ public class CountLettersTest {
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void onlyOneWordDoesMatch() {
+        int words = new CountLetters().count("catr");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void onlyOneWordDoesNotMatch() {
+        int words = new CountLetters().count("cat");
+        Assertions.assertEquals(0, words);
+    }
+
+    @Test
+    public void multipleWordsFirstEndsWithr() {
+        int words = new CountLetters().count("catr!dogs");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void onlySecondWordMatches() {
+        int words = new CountLetters().count("cat!dogs");
+        Assertions.assertEquals(1, words);
+    }
+
 }

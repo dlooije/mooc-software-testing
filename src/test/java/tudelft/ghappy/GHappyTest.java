e8556e1a7658ebc1f7f87a06b0730dd24a08b876
Daphne Looije <Lje@cqm.nl>
2021-02-12 13:53:26 +0100

    HappyG tests and bugfix

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,23 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "string = {0}")
+    @CsvSource({"gg", "aggg", "aggaggaggb", "a"})
+    public void testHappyG(String inp) {
+        GHappy gHappy = new GHappy();
+        Assertions.assertTrue(gHappy.gHappy(inp));
+    }
+
+    @ParameterizedTest(name = "string = {0}, happyg = {1}")
+    @CsvSource({"g", "aggaga", "aggaggaggbg"})
+    public void testNonHappyG(String inp) {
+        GHappy gHappy = new GHappy();
+        Assertions.assertFalse(gHappy.gHappy(inp));
+    }
 }
